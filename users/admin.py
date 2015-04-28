a97ce97bcf2f5db27e0793510acff14fe6267cfb
ericmarcos <ericmarcos.p@gmail.com>
2015-04-28 12:22:52 +0200

    updated user admin with inline profiles and devices

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import DareyooUserProfile, Device
 
-# Register your models here.
+admin.site.unregister(User)
+
+class DareyooUserProfileInline(admin.StackedInline):
+    model = DareyooUserProfile
+    fk_name = 'user'
+
+class DeviceInline(admin.StackedInline):
+    model = Device
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [DareyooUserProfileInline, DeviceInline, ]
+
+admin.site.register(User, UserProfileAdmin)
