0d7506454e10db634be6b930b64eb96c5c0543a7
ericmarcos <ericmarcos.p@gmail.com>
2015-04-29 18:11:22 +0200

    added game admin models

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PoolResultInline(admin.StackedInline):
+    model = PoolResult
+    extra = 0
+
+
+class PoolOptionInline(admin.StackedInline):
+    model = PoolOption
+    extra = 0
+
+
+class PoolAdmin(admin.ModelAdmin):
+    inlines = [PoolResultInline, PoolOptionInline, ]
+
+
+admin.site.register(Pool, PoolAdmin)
+admin.site.register(Team)
+
+
+class PrizeInline(admin.StackedInline):
+    model = Prize
+    extra = 0
+
+
+class FixtureInline(admin.StackedInline):
+    model = Fixture
+    extra = 0
+
+
+class LeagueAdmin(admin.ModelAdmin):
+    inlines = [PrizeInline, FixtureInline, ]
+
+
+admin.site.register(League, LeagueAdmin)
