e2c0701d7b59e50343ffb88f392f7c8e447ebe83
ericmarcos <ericmarcos.p@gmail.com>
2015-05-29 17:56:35 +0200

    fixed admin static media broken links

--- dareyoo2/settings.py
+++ dareyoo2/settings.py
@@ -177,6 +177,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_HOST = os.environ.get('AWS_S3_HOST')
+AWS_QUERYSTRING_AUTH = False
 
 ### CORS ###
 CORS_URLS_REGEX = r'^(/api/.*|/auth/.*)$'
