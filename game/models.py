664d26d80e6876437206af68ff8db3eaf91b15dd
ericmarcos <ericmarcos.p@gmail.com>
2015-07-28 10:45:40 +0200

    fixed pool options ordering

--- game/models.py
+++ game/models.py
@@ -254,7 +254,7 @@ class Pool(models.Model):
     def __unicode__(self):
         if self.title:
             return unicode(self.title)
-        options = list(self.options.all().reverse())
+        options = list(self.options.all())
         if len(options) > 0:
             return " - ".join(map(unicode, options))
         return "Pool %s" % self.id
@@ -307,6 +307,7 @@ class PoolOption(models.Model):
 
     class Meta:
         app_label = 'game'
+        ordering = ['id',]
 
 
 class PoolResult(models.Model):
