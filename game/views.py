254735a8210d805ab545e2f4a4f570f207c3dd83
ericmarcos <ericmarcos.p@gmail.com>
2015-08-01 20:47:57 +0200

    added extra_points metadata (missing view parameters)

--- game/views.py
+++ game/views.py
@@ -243,7 +243,7 @@ class LeagueViewSet(viewsets.ModelViewSet):
     def extra_points(self, request, pk=None):
         league = self.get_object()
         try:
-            league.extra_points(request.user)
+            league.extra_points(request.user, request.data.get('type'), request.data.get('data'))
         except Exception as e:
             raise ParseError(detail=str(e))
         return Response({'status': 'User won +2 extra points'})
