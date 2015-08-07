d026f427d46c3c5ed5a29e42d0b57e943c3780d3
ericmarcos <ericmarcos.p@gmail.com>
2015-08-07 19:20:30 +0200

    set debug to env var

--- teamwin/settings.py
+++ teamwin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's_+3wxw#a86nsx@y(7yjcya#rt7eek3j)&^hcpi(s25lqy4#41'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [
     'dareyoo.herokuapp.com',
