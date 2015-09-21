426b07e4cd1aea577c0e75f48206766330de3fd0
ericmarcos <ericmarcos.p@gmail.com>
2015-09-21 16:38:19 +0200

    added terms and conditions

--- teamwin/urls.py
+++ teamwin/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^auth/', include('rest_framework_social_oauth2.urls')),
     url(r'^api/', include('game.urls')),
     url(r'^$', TemplateView.as_view(template_name="landing.html"), name='landing'),
+    url(r'^terms_conditions$', TemplateView.as_view(template_name="terms_conditions.html"), name='terms_conditions'),
 ]
